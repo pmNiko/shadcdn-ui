feb3a7611589fa3a035e119a875a969890db25a6
nikodev <martin.panebianco@smandes.gob.ar>
2024-08-06 13:47:19 -0300

    Add acordion

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
+
+const items = [
+  {
+    id: "Item-1",
+    question: "Is it accesible?",
+    answer: "Yes.It adheres to the WAI-ARIA design patters."
+  },
+  {
+    id: "Item-2",
+    question: "Can I use It on multiple projects?",
+    answer: "Yes. It licensed under the MIT license."
+  },
+  {
+    id: "Item-3",
+    question: "How to do I get started?",
+    answer: "You can start by reading the documentation."
+  },
+  {
+    id: "Item-4",
+    question: "How to do I get started?",
+    answer: "You can start by reading the documentation."
+  },
+]
 
 const page = () => {
   return (
-    <div>Accordion</div>
+    <Accordion type="multiple"  className="w-full">
+      {
+        items.map(item => (
+          <AccordionItem value={item.id}>
+          <AccordionTrigger>{item.question}</AccordionTrigger>
+          <AccordionContent>
+            {item.answer}
+          </AccordionContent>
+        </AccordionItem>
+        ))
+      }
+  
+  </Accordion>
   )
 }
 
